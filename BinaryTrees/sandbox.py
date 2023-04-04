782500806ff92746c262807c9b9c844574f34c29
arsalon-amini <arsalonamini2020@gmail.com>
2023-04-04 13:43:08 +0800

    iterative solution using stack

--- BinaryTrees/sandbox.py
+++ BinaryTrees/sandbox.py
@@ -1,11 +1,4 @@
 # given two binary trees, merge them, if two nodes overlap sum the values
-"""
-Result: took 25 minutes, I needed help on edge cases (if nodes are null), in end, I am still confused on what happens
-on call stack when tree_one is None and we "return tree_two" 
-how does that get appended (mutate) tree_one so that when we return tree_one the node is linked
-ANSWER: I thought about it and realized we're setting tree_one.left = recursivefn(tree_one.left, tree_two.left) 
-so if we return tree_two it will get assigned as the tree_one.left = tree_two_node on that line
-"""
 
 # tree_one =
 #      1
@@ -45,3 +38,57 @@ def merge_trees(tree_one, tree_two):
     tree_one.left = merge_trees(tree_one.left, tree_two.left)
     tree_one.right = merge_trees(tree_one.right, tree_two.right)
     return tree_one
+
+# Iterative Solution ~
+# DFS - use a stack for visiting nodes in tree
+
+
+def merge_bts(tree_one, tree_two):
+    if tree_one is None:
+        return tree_two
+
+    stack_one = [tree_one]
+    stack_two = [tree_two]
+
+    while len(stack_one) > 0:
+        tree_one_node = stack_one.pop()
+        tree_two_node = stack_two.pop()
+
+        # handle edge cases ~
+        if tree_two_node is None:
+            continue
+
+        tree_one_node.value += tree_two_node.value
+
+        if tree_one_node.left is None:
+            tree_one_node.left = tree_two_node.left
+        else:
+            stack_one.append(tree_one_node.left)
+            stack_two.append(tree_two_node.left)
+
+        if tree_one_node.right is None:
+            tree_one_node.right = tree_two_node.right
+        else:
+            stack_one.append(tree_one_node.right)
+            stack_two.append(tree_two_node.right)
+
+    return tree_one
+
+# tree_one =
+#      1
+#   3     2
+#  7  4
+
+# tree_two =
+#     1
+#   5    9
+# 2     7  6
+
+# stack_one = [ ]
+# stack_two = [ ]
+
+
+# merged_tree =
+#    2
+#  8     11
+# 9  4  7  6
